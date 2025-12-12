330e2434a37f7a29419d1f6a509b0e1a5085e253
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-12-12 15:13:34 +0100

    add conectivity between NestJS and Prisma/Postgres

--- api/src/health/health.controller.ts
+++ api/src/health/health.controller.ts
@@ -1,9 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { PrismaService } from 'prisma/prisma.service';
 
 @Controller('health')
 export class HealthController {
+  constructor(private readonly prisma: PrismaService) {}
   @Get()
   checkHealth() {
     return { status: 'ok' };
+  }
+
+  @Get('db')
+  async getDbHealth() {
+    //Connectivity check
+    await this.prisma.$queryRaw`SELECT 1`;
+    return { status: 'db ok' };
   }
 }
